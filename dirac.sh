baea69b77bcc651367071ed9371bf316cd035c5b
Dicky Herlambang (花） <herlambangdicky5@gmail.com>
2019-02-28 10:03:42 +0700

    Update DIRAC configuration for XenonHD
    Make the installer know if this will installed on xenonhd, syberia or other ROMs and place specific files correctly.
    
    Updated audio_effects.xml for xenonhd to implement DIRAC (on testing)

--- dirac.sh
+++ dirac.sh
@@ -3,62 +3,53 @@
 
 #Mounting Your System
 	mount -o remount,rw /system
-	
-#Backup your stock libs file
-if [ "`grep ro.product.name=syberia_mido /system/build.prop`" ];
-then 
-	mkdir /sdcard/backup
-	mkdir /sdcard/backup/lib
-	mkdir /sdcard/backup/lib64
-	mkdir /sdcard/backup/vendor
-	mkdir /sdcard/backup/vendor/etc
-	cp /system/lib/libaudioclient.so /sdcard/backup/lib/libaudioclient.so
-	cp /system/lib/libaudioeffect_jni.so /sdcard/backup/lib/libaudioeffect_jni.so
-	cp /system/lib/libaudioflinger.so /sdcard/backup/lib/libaudioflinger.so
-	cp /system/lib/libaudiopolicyenginedefault.so /sdcard/backup/lib/libaudiopolicyenginedefault.so
-	cp /system/lib/libaudiopolicymanager.so /sdcard/backup/lib/libaudiopolicymanager.so
-	cp /system/lib/libaudiopolicymanagerdefault.so /sdcard/backup/lib/libaudiopolicymanagerdefault.so 
-	cp /system/lib/libaudiopolicyservice.so /sdcard/backup/lib/libaudiopolicyservice.so
-	cp /system/lib/libaudioprocessing.so /sdcard/backup/lib/libaudioprocessing.so
-	cp /system/lib/libsoundpool.so /sdcard/backup/lib/libsoundpool.so
-	cp /system/lib/libsoundtrigger.so /sdcard/backup/lib/libsoundtrigger.so
-	cp /system/lib/libsoundtriggerservice.so /sdcard/backup/lib/libsoundtriggerservice.so
-	cp /system/lib64/libaudioclient.so /sdcard/backup/lib64/libaudioclient.so
-	cp /system/lib64/libaudiopolicyenginedefault.so /sdcard/backup/lib64/libaudiopolicyenginedefault.so
-	cp /system/lib64/libaudiopolicymanager.so /sdcard/backup/lib64/libaudiopolicymanager.so
-	cp /system/lib64/libaudiopolicymanagerdefault.so /sdcard/backup/lib64/libaudiopolicymanagerdefault.so
-	cp /system/lib64/libaudiopolicyservice.so /sdcard/backup/lib64/libaudiopolicyservice.so
-	cp /system/lib64/libaudioroute.so /sdcard/backup/lib64/libaudioroute.so 
-	cp /vendor/etc/audio_effects.xml /sdcard/backup/vendor/etc/audio_effects.xml
-	cp /vendor/etc/audio_output_policy.conf /sdcard/backup/vendor/etc/audio_output_policy.conf
-	cp /vendor/etc/audio/audio_policy.conf /sdcard/backup/vendor/etc/audio/audio_policy.conf
-else
-	mkdir /sdcard/backup
-	mkdir /sdcard/backup/lib
-	mkdir /sdcard/backup/lib64
-	mkdir /sdcard/backup/vendor
-	mkdir /sdcard/backup/vendor/etc
-	cp /system/lib/libaudioclient.so /sdcard/backup/lib/libaudioclient.so
-	cp /system/lib/libaudioeffect_jni.so /sdcard/backup/lib/libaudioeffect_jni.so
-	cp /system/lib/libaudioflinger.so /sdcard/backup/lib/libaudioflinger.so
-	cp /system/lib/libaudiopolicyenginedefault.so /sdcard/backup/lib/libaudiopolicyenginedefault.so
-	cp /system/lib/libaudiopolicymanager.so /sdcard/backup/lib/libaudiopolicymanager.so
-	cp /system/lib/libaudiopolicymanagerdefault.so /sdcard/backup/lib/libaudiopolicymanagerdefault.so 
-	cp /system/lib/libaudiopolicyservice.so /sdcard/backup/lib/libaudiopolicyservice.so
-	cp /system/lib/libaudioprocessing.so /sdcard/backup/lib/libaudioprocessing.so
-	cp /system/lib/libsoundpool.so /sdcard/backup/lib/libsoundpool.so
-	cp /system/lib/libsoundtrigger.so /sdcard/backup/lib/libsoundtrigger.so
-	cp /system/lib/libsoundtriggerservice.so /sdcard/backup/lib/libsoundtriggerservice.so
-	cp /system/lib64/libaudioclient.so /sdcard/backup/lib64/libaudioclient.so
-	cp /system/lib64/libaudiopolicyenginedefault.so /sdcard/backup/lib64/libaudiopolicyenginedefault.so
-	cp /system/lib64/libaudiopolicymanager.so /sdcard/backup/lib64/libaudiopolicymanager.so
-	cp /system/lib64/libaudiopolicymanagerdefault.so /sdcard/backup/lib64/libaudiopolicymanagerdefault.so
-	cp /system/lib64/libaudiopolicyservice.so /sdcard/backup/lib64/libaudiopolicyservice.so
-	cp /system/lib64/libaudioroute.so /sdcard/backup/lib64/libaudioroute.so 
-fi
 
 #Remove DIRAC Audio Effetcs From System
+if [ "`grep ro.product.name=xenonhd_mido /system/build.prop`" ];
+then 
+	echo "DIRAC NOT REMOVED"
+	echo "But changed to revision V10.2.3.0.NCFMIXM"
+	rm /system/lib/libDiracAPI_SHARED.so
+	rm /system/lib/soundfx/libdirac.so
+	rm /system/vendor/etc/diracmobile.config
+	cp /temp/propietary/lib/libDiracAPI_SHARED.so /system/lib/libDiracAPI_SHARED.so
+	cp /temp/propietary/lib/soundfx/libdirac.so /system/lib/soundfx/libdirac.so
+	cp /temp/propietary/vendor/etc/diracmobile.config /system/vendor/etc/diracmobile.config
+	chmod 0644 /system/lib/libDiracAPI_SHARED.so
+	chmod 0644 /system/lib/soundfx/libdirac.so
+	chmod 0644 /system/vendor/etc/diracmobile.config
+	if [ "`grep persist.audio.dirac.speaker=true /system/vendor/build.prop`" ];
+	then
+		echo "DIRAC Speaker not written"
+	else
+		echo "persist.audio.dirac.speaker=true" >> /system/vendor/build.prop
+	fi
+else
 	rm /system/lib/soundfx/libdirac.so
 	rm /system/lib/libDiracAPI_SHARED.so
 	rm /system/vendor/etc/diracmobile.config
 	sed -i '/persist.audio.dirac.speaker=true/d' /vendor/build.prop
+fi
+	
+#Copy modified audio_effects.xml for syberia os only 
+#Syberia have updated this config but other ROMs not, so copy only for preconfigured syberia only
+#Will check other ROM later.
+if [ "`grep ro.product.name=syberia_mido /system/build.prop`" ];
+then 
+    rm /system/vendor/etc/audio_effects.xml
+	cp /system/vendor/etc/syberia_audio_effects.xml /system/vendor/etc/audio_effects.xml
+	chmod 0644 /system/vendor/etc/audio_effects.xml
+	rm /system/vendor/etc/syberia_audio_effects.xml
+fi
+if [ "`grep ro.product.name=xenonhd_mido /system/build.prop`" ];
+then 
+	rm /system/vendor/etc/audio_effects.xml
+	cp /system/vendor/etc/xenon_audio_effects.xml /system/vendor/etc/audio_effects.xml
+	chmod 0644 /system/vendor/etc/audio_effects.xml
+	rm /system/vendor/etc/default_audio_effects.xml
+else
+	rm /system/vendor/etc/audio_effects.xml
+	cp /system/vendor/etc/default_audio_effects.xml /system/vendor/etc/audio_effects.xml
+	chmod 0644 /system/vendor/etc/audio_effects.xml
+	rm /system/vendor/etc/default_audio_effects.xml
+fi
