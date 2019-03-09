0aa767a49789e39f379fa46a85b090ac06f175ce
Dicky Herlambang (花） <herlambangdicky5@gmail.com>
2019-03-09 14:11:51 +0700

    Merge update from "Magisk" Branch
    Merge all updates from Magisk branch to master branch since master branch is really outdated.
    
    What's merged:
    - Revert 24bit change as global usage
    - Updated audio config from syberia
    - Completely remove "RAW" flags from system
    - Import aptx blobs from mata
    - Updated documentation
    
    And real changes on this branch :
    - Merge propietary files to each system or vendor since dirac problem already solved
    - Only use 1 audio_effects.xml for all ROMs
    - Change compatibility to all ROMs and not limited for specific ROMs

--- README.md
+++ README.md
@@ -2,16 +2,16 @@
 
 This is a flashable zip that containt several libs to restore capatibility of accessing to internal DAC (DIRECT/DIRECT_PCM) from audio policy on Mido devices after getting updates to latest Pie versions.
 
-This package will restore all lib,libs64 and audio_effects.xml to vendor/etc to your system, remove all DIRAC Audio Effects from over the system (except for xenonhd ROM) and install updated audio caf pre-built effects to your system.
+This package will restore all lib,libs64 and updated audio config to your system and install updated audio caf pre-built effects to your system.
 
-NOTE: If any other ROM still not implemented DIRAC on settings, then it should get back DIRAC audio effects on system, i'll try to search if possible, or then someone can pm me if they need this.
+NOTE: This package was created for almost all pie ROMs for Mido and if anyone want to use magisk version then check [Master Branch](https://github.com/Nicklas373/Internal_DAC_Fixer/tree/Magisk).
 
-This package only have aim to restore internal DAC capatibility as bare minimum lib without any harsh modification, no other else and not have any means to insult anyone that work with mido since this is only for my personal use. (Anyone can use it, but DWYOR)
+This package only have aim to restore internal DAC capatibility include with several libs to optimize audio experience without any harsh modification and not have any means to insult anyone that work with Mido since this is only for my personal use. (Anyone can use it, but DWYOR)
 
 # Project Status
 ![Build](https://img.shields.io/badge/Build%20Status-Updated-green.svg) ![Version](https://img.shields.io/badge/Latest%20Version-0.7-blue.svg) ![Library](https://img.shields.io/badge/Library%20Base-LiquidRemix%2020181220%20-blue.svg) ![Package](https://img.shields.io/badge/Package-flashable%20zip-blue.svg)
 
-What's lib that get replaced :
+What's lib that get replaced (Core library) :
 - /system/lib/libaudioclient.so
 - /system/lib/libaudioeffect_jni.so
 - /system/lib/libaudioflinger.so
@@ -23,22 +23,23 @@ What's lib that get replaced :
 - /system/lib/libsoundpool.so
 - /system/lib/libsoundtrigger.so
 - /system/lib/libsoundtriggerservice.so
-- /vendor/etc/audio_effects.xml
-- /vendor/etc/audio_output_policy.conf
-- /vendor/etc/audio_platform_info.xml
-- /vendor/etc/audio_policy.conf
-- /vendor/etc/audio_policy_configuration.xml
-- /vendor/etc/mixer_paths_mtp.xml
 
-What's lib that removed :
+Additional library :
 - /system/lib/soundfx/libdirac.so
 - /system/lib/libDiracAPI_SHARED.so
-- /system/vendor/etc/diracmobile.config
-- String "/persist.audio.dirac.speaker=true/d" on vendor/build.prop
+- /system/lib64/libaptXHD_encoder.so
+- /system/lib64/libaptX_encoder.so
+- /vendor/etc/audio_effects.xml
+- /vendor/etc/audio_policy.conf
+- /vendor/etc/audio_platform_info.xml
+- /vendor/etc/diracmobile.config
+- /vendor/etc/mixer_paths_mtp.xml
 
-Compatibitily ROM of this patch (so far that i tested):
+Compatibitily ROM of this patch (This for already tested):
 - XenonHD Experimental Build
 - Syberia OS Official Build
+- Pixel Experience CAF Official Build (should work for AOSP build too)
+- LineageOS 16 Unofficial Build
 
 Other Information Catalog:
 - [FAQ](https://github.com/Nicklas373/Internal_DAC_Fixer/blob/master/docs/FAQ.md)
--- dirac.sh
+++ dirac.sh
@@ -4,73 +4,10 @@
 #Mounting Your System
 	mount -o remount,rw /system
 
-#Remove DIRAC Audio Effetcs From System
-if [ "`grep ro.product.name=xenonhd_mido /system/build.prop`" ];
-then 
-	echo "DIRAC NOT REMOVED"
-	echo "But changed to revision V10.2.3.0.NCFMIXM"
-	rm /system/lib/libDiracAPI_SHARED.so
-	rm /system/lib/soundfx/libdirac.so
-	rm /system/vendor/etc/diracmobile.config
-	cp /temp/propietary/lib/libDiracAPI_SHARED.so /system/lib/libDiracAPI_SHARED.so
-	cp /temp/propietary/lib/soundfx/libdirac.so /system/lib/soundfx/libdirac.so
-	cp /temp/propietary/vendor/etc/diracmobile.config /system/vendor/etc/diracmobile.config
-	chmod 0644 /system/lib/libDiracAPI_SHARED.so
-	chmod 0644 /system/lib/soundfx/libdirac.so
-	chmod 0644 /system/vendor/etc/diracmobile.config
+#Check if speaker enable or not on system
 	if [ "`grep persist.audio.dirac.speaker=true /system/vendor/build.prop`" ];
 	then
 		echo "DIRAC Speaker not written"
 	else
 		echo "persist.audio.dirac.speaker=true" >> /system/vendor/build.prop
 	fi
-else
-	rm /system/lib/soundfx/libdirac.so
-	rm /system/lib/libDiracAPI_SHARED.so
-	rm /system/vendor/etc/diracmobile.config
-	sed -i '/persist.audio.dirac.speaker=true/d' /vendor/build.prop
-fi
-	
-#Copy modified audio_effects.xml for syberia os only.
-#Syberia have updated this config but other ROMs not, so copy only for preconfigured syberia only.
-#Will check other ROM later.
-#Copy Modified audio config from syberia os, but not copy same config to syberia rom.
-if [ "`grep ro.product.name=syberia_mido /system/build.prop`" ];
-then 
-    rm /system/vendor/etc/audio_effects.xml
-	cp /system/vendor/etc/syberia_audio_effects.xml /system/vendor/etc/audio_effects.xml
-	chmod 0644 /system/vendor/etc/audio_effects.xml
-	rm /system/vendor/etc/syberia_audio_effects.xml
-fi
-if [ "`grep ro.product.name=xenonhd_mido /system/build.prop`" ];
-then 
-	rm /system/vendor/etc/audio_effects.xml
-	cp /system/vendor/etc/xenon_audio_effects.xml /system/vendor/etc/audio_effects.xml
-	chmod 0644 /system/vendor/etc/audio_effects.xml
-	rm /system/vendor/etc/default_audio_effects.xml
-	cp /temp/propietary/vendor/etc/audio_output_policy.conf /system/vendor/etc/audio_output_policy.conf
-	cp /temp/propietary/vendor/etc/audio_platform_info.xml /system/vendor/etc/audio_platform_info.xml
-	cp /temp/propietary/vendor/etc/audio_policy.conf /system/vendor/etc/audio_policy.conf
-	cp /temp/propietary/vendor/etc/audio_policy_configuration.xml /system/vendor/etc/audio_policy_configuration.xml
-	cp /temp/propietary/vendor/etc/mixer_paths_mtp.xml /system/vendor/etc/mixer_paths_mtp.xml
-	chmod 0644 /system/vendor/etc/audio_output_policy.conf
-	chmod 0644 /system/vendor/etc/audio_platform_info.xml
-	chmod 0644 /system/vendor/etc/audio_policy.conf
-	chmod 0644 /system/vendor/etc/audio_policy_configuration.xml
-	chmod 0644 /system/vendor/etc/mixer_paths_mtp.xml
-else
-	rm /system/vendor/etc/audio_effects.xml
-	cp /system/vendor/etc/default_audio_effects.xml /system/vendor/etc/audio_effects.xml
-	chmod 0644 /system/vendor/etc/audio_effects.xml
-	rm /system/vendor/etc/default_audio_effects.xml
-	cp /temp/propietary/vendor/etc/audio_output_policy.conf /system/vendor/etc/audio_output_policy.conf
-	cp /temp/propietary/vendor/etc/audio_platform_info.xml /system/vendor/etc/audio_platform_info.xml
-	cp /temp/propietary/vendor/etc/audio_policy.conf /system/vendor/etc/audio_policy.conf
-	cp /temp/propietary/vendor/etc/audio_policy_configuration.xml /system/vendor/etc/audio_policy_configuration.xml
-	cp /temp/propietary/vendor/etc/mixer_paths_mtp.xml /system/vendor/etc/mixer_paths_mtp.xml
-	chmod 0644 /system/vendor/etc/audio_output_policy.conf
-	chmod 0644 /system/vendor/etc/audio_platform_info.xml
-	chmod 0644 /system/vendor/etc/audio_policy.conf
-	chmod 0644 /system/vendor/etc/audio_policy_configuration.xml
-	chmod 0644 /system/vendor/etc/mixer_paths_mtp.xml
-fi
