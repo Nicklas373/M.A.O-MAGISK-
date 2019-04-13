231f16a54a02376a5a8f4e468b41cad655a69add
Dicky Herlambang (花） <herlambangdicky5@gmail.com>
2019-04-13 20:53:30 +0700

    Try to push Audio High Performance Mode

--- common/service.sh
+++ common/service.sh
@@ -7,3 +7,15 @@
 MODDIR=${0%/*}
 
 # This script will be executed in late_start service mode
+# Let it sleep first for 30 second
+sleep 30
+
+# Set permission for Audio High Performance Mode
+chmod 0644 /sys/module/snd_soc_wcd9330/parameters/high_perf_mode
+chmod 0644 /sys/module/snd_soc_wcd9335/parameters/huwifi_mode
+chmod 0644 /sys/module/snd_soc_wcd9xxx/parameters/impedance_detect_en
+
+# Active Audio High Perfomance Mode
+echo "1" > /sys/module/snd_soc_wcd9330/parameters/high_perf_mode
+echo "1" > /sys/module/snd_soc_wcd9335/parameters/huwifi_mode
+echo "1" > /sys/module/snd_soc_wcd9xxx/parameters/impedance_detect_en
