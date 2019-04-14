8c4ac8125fec228fa347f1e4a0fdbc15d6de8a04
Dicky Herlambang (花） <herlambangdicky5@gmail.com>
2019-04-14 08:57:21 +0700

    Disable Audio High Performance by default
    Audio High Performance mode is work and can be active, but if it active by default then some performance degredation will happen on high gaming or high perfomance usage.
    
    So switch it to disable by default and let userspace configure it using script by itself
    
    |*| Introduce UHQA binary script, to configure and check exist path status from sound card directly on kernel.
    
    How to use:
    su
    uhqa
    
    and try to follow guide inside script.

--- common/service.sh
+++ common/service.sh
@@ -11,15 +11,15 @@ MODDIR=${0%/*}
 # 1. Enable Headset High Perfomance Mode (HPH v2)
 # 2. Enable Ultra High Quality Audio Mode (UHQA Mode)
 # 3. Enable Headphone Impedance Detection
+
 # Let it sleep first for 30 second
 sleep 30
 
-# Set permission for Audio High Performance Mode
-chmod 0644 /sys/module/snd_soc_wcd9330/parameters/high_perf_mode
-chmod 0644 /sys/module/snd_soc_wcd9335/parameters/huwifi_mode
-chmod 0644 /sys/module/snd_soc_wcd9xxx/parameters/impedance_detect_en
+# Disable Audio High Perfomance Mode by default on boot
+# Let userspace script configure it by itself (It can reduce some lag when on gaming or high usage)
+echo "0" > /sys/module/snd_soc_wcd9330/parameters/high_perf_mode
+echo "0" > /sys/module/snd_soc_wcd9335/parameters/huwifi_mode
+echo "0" > /sys/module/snd_soc_wcd9xxx/parameters/impedance_detect_en
 
-# Active Audio High Perfomance Mode
-echo "1" > /sys/module/snd_soc_wcd9330/parameters/high_perf_mode
-echo "1" > /sys/module/snd_soc_wcd9335/parameters/huwifi_mode
-echo "1" > /sys/module/snd_soc_wcd9xxx/parameters/impedance_detect_en
+# Set permission for Audio High Performance Mode Scripts
+chmod 0777 /system/bin/uhqa
