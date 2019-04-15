877391c91edba603e982af7654f4c570416bf02c
Dicky Herlambang (花） <herlambangdicky5@gmail.com>
2019-04-15 12:15:24 +0700

    UHQA: Fix mounting & ROM detection decision issue

--- common/service.sh
+++ common/service.sh
@@ -16,6 +16,12 @@ MODDIR=${0%/*}
 # Let it sleep first for 30 second
 sleep 30
 
+# Set permission for Audio High Performance Mode
+chmod 0644 /sys/module/snd_soc_wcd9330/parameters/high_perf_mode
+chmod 0644 /sys/module/snd_soc_wcd9335/parameters/huwifi_mode
+chmod 0644 /sys/module/snd_soc_wcd9335/parameters/low_distort_amp
+chmod 0644 /sys/module/snd_soc_wcd9xxx/parameters/impedance_detect_en
+
 # Disable Audio High Perfomance Mode by default on boot
 # Let userspace script configure it by itself (It can reduce some lag when on gaming or high usage)
 echo "0" > /sys/module/snd_soc_wcd9330/parameters/high_perf_mode
