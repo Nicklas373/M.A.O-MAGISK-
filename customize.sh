820c12c9157ebcb416a95225a083fabbc261ca9c
Dicky Herlambang <herlambangdicky5@gmail.com>
2020-01-16 22:54:43 +0700

    M.A.O: update depedencies for Q
    Signed-off-by: Dicky Herlambang <herlambangdicky5@gmail.com>

--- customize.sh
+++ customize.sh
@@ -1,7 +1,7 @@
 ui_print "                                 "
 ui_print "******Mido Audio Optimizer*******"
 ui_print "*                               *"
-ui_print "*    For Pie ROM Based Only     *"
+ui_print "*    For Q ROM Based Only       *"
 ui_print "*                               *"
 ui_print "*********************************"
 ui_print "                                 "
@@ -17,19 +17,19 @@ ui_print " "
 
 if [ $DEVICE == "mido" ];
 	then
-		if [ $API == "28" ];
+		if [ $API == "29" ];
 			then
 				ui_print " "
 				ui_print "****************** Software ********************"
 				ui_print "Custom Package  : Mido Audio Optimizer          "
-				ui_print "Android version : 9.0.x                         "
-				ui_print "SDK version     : 28                            "
+				ui_print "Android version : 10                            "
+				ui_print "SDK version     : 29                            "
 				ui_print "Build date      : Thu Jan 16 22:26:20 GMT+7 2019"
-				ui_print "Build ID        : 1.0.0                         "
+				ui_print "Build ID        : 1.0.2                         "
 				ui_print "Developed By    : Dicky Herlambang              "
 				ui_print "************************************************"
 				ui_print " "
-		elif [ $API != "28" ];
+		elif [ $API != "29" ];
 			then
 				 abort "Your API Level '"$API"' is not supported yet"
 		fi
