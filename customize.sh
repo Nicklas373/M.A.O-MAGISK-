eb4a76a819cd590fcff66964d01f853e660b8af5
Dicky Herlambang <herlambangdicky5@gmail.com>
2020-04-02 16:56:28 +0700

    M.A.O: Update build-date
    Signed-off-by: Dicky Herlambang <herlambangdicky5@gmail.com>

--- customize.sh
+++ customize.sh
@@ -24,7 +24,7 @@ if [ $DEVICE == "mido" ];
 				ui_print "Custom Package  : Mido Audio Optimizer          "
 				ui_print "Android version : 10                            "
 				ui_print "SDK version     : 29                            "
-				ui_print "Build date      : Thu Jan 16 22:26:20 GMT+7 2019"
+				ui_print "Build date      : Thu Apr 02 16:55:20 GMT+7 2020"
 				ui_print "Build ID        : 1.0.2                         "
 				ui_print "Developed By    : Dicky Herlambang              "
 				ui_print "************************************************"
